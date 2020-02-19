1bf13bcd5d43c2936786672c6eba3aeb94c549c9
Fixer <fixer3838@live.fr>
2020-02-19 12:04:48 +0100

    Adding from function for Opcode enum to get opcode from a vec of u8

--- src/instruction.rs
+++ src/instruction.rs
@@ -4,6 +4,15 @@ pub enum Opcode {
     IGL
 }
 
+impl From<u8> for Opcode {
+    fn from(v: u8) -> Self {
+        match v {
+            0 => Opcode::HLT,
+            _ => Opcode::IGL,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct Instruction {
     opcode: Opcode
