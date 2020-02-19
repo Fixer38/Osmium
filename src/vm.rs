3205a23f2501b9e7eb0005849a9acc60d9d5c4ff
Fixer <fixer3838@live.fr>
2020-02-19 12:07:56 +0100

    Added pc and program fields for struct VM, modifying new method to add default values for the pc and program, added run and decode_opcode method to run the vm and decode incoming opcode given the program vec. Added test to create a random opcode and test out run/decode_opcode

--- src/vm.rs
+++ src/vm.rs
@@ -1,13 +1,44 @@
+use super::instruction::*;
+
 pub struct VM {
     registers: [i32; 32],
+    pc: usize,
+    program: Vec<u8>
 }
 
 
 impl VM {
     pub fn new() -> VM {
         VM {
-            registers: [0; 32]
+            registers: [0; 32],
+            pc: 0,
+            program: vec![],
         }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            // break the program loop if pc gets higher than the amount of bytes allowed
+            if self.pc >= self.program.len() {
+                break
+            }
+        }
+        match self.decode_opcode() {
+            Opcode::HLT => {
+                println!("Halt encountered");
+                return;
+            },
+            _ => {
+                println!("Opcode not recognized");
+                return;
+            }
+        }
+    }
+
+    fn decode_opcode(&mut self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+        self.pc += 1;
+        opcode
     }
 }
 
@@ -19,6 +50,16 @@ mod tests {
     #[test]
     fn test_create_vm() {
         let test_vm = VM::new();
-        assert_eq!(test_vm.registers[0], 0)
+        assert_eq!(test_vm.registers[0], 0);
+        assert_eq!(test_vm.pc, 0);
+        assert!(test_vm.program.is_empty());
+    }
+
+    #[test]
+    fn test_opcode_hlt() {
+        let mut test_vm = VM::new();
+        test_vm.program = vec![20, 0, 0, 0];
+        test_vm.run();
+        assert_eq!(test_vm.pc, 1);
     }
 }
