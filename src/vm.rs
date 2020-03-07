1034e22ce315615dd0b2fd42c0fba511a7f60aff
Fixer <fixer3838@live.fr>
2020-03-07 20:30:54 +0100

    Fixed test with execute_instruction (a bit messy as it is now => to refractor later)

--- src/vm.rs
+++ src/vm.rs
@@ -3,7 +3,8 @@ use super::instruction::*;
 pub struct VM {
     registers: [i32; 32],
     pc: usize,
-    program: Vec<u8>
+    program: Vec<u8>,
+    remainder: u32,
 }
 
 
@@ -13,6 +14,7 @@ impl VM {
             registers: [0; 32],
             pc: 0,
             program: vec![],
+            remainder: 0,
         }
     }
 
@@ -64,6 +66,16 @@ impl VM {
                 let operand2 = self.registers[self.next_8_bits() as usize];
                 self.registers[self.next_8_bits() as usize] = operand1 * operand2;
             }
+            Opcode::DIV => {
+                // DIV format: $1, $2, $3
+                // $1 and $2 operands to multiply from register
+                // $3 division quotient destination register
+                // Remainder will contain remainder of the division => i32 allowed in the registers
+                let operand1 = self.registers[self.next_8_bits() as usize];
+                let operand2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = operand1 / operand2;
+                self.remainder = (operand1 % operand2) as u32;
+            }
             Opcode::IGL => println!("unknown instruction encountered")
         }
         true
@@ -107,7 +119,7 @@ mod tests {
     fn test_opcode_hlt() {
         let mut test_vm = VM::new();
         test_vm.program = vec![0, 0, 0, 0];
-        test_vm.run();
+        test_vm.execute_instruction();
         assert_eq!(test_vm.pc, 1);
     }
 
@@ -115,7 +127,7 @@ mod tests {
     fn test_opcode_igl() {
         let mut test_vm = VM::new();
         test_vm.program = vec![200, 0, 0, 0];
-        test_vm.run();
+        test_vm.execute_instruction();
         assert_eq!(test_vm.pc, 1);
     }
 
@@ -123,15 +135,43 @@ mod tests {
     fn test_opcode_load() {
         let mut test_vm = VM::new();
         test_vm.program = vec![1, 0, 1, 244]; // 1 in first for the LOAD. 1 and 244 = 500 (LE)
-        test_vm.run();
+        test_vm.execute_instruction();
         assert_eq!(test_vm.registers[0], 500); // Expected result to be 500
     }
 
     #[test]
     fn test_opcode_add() {
         let mut test_vm = VM::new();
-        test_vm.program = vec![2, 0, 2, 255];
-        test_vm.run();
+        // Store 255 in register 0
+        test_vm.program = vec![1, 0, 0, 255];
+        test_vm.execute_instruction();
+        test_vm.pc = 0;
+        // Store 2 in register 1
+        test_vm.program = vec![1, 1, 0, 2];
+        test_vm.execute_instruction();
+        test_vm.pc = 0;
+        // Add the 2 and check results
+        test_vm.program = vec![2, 0, 1, 0];
+        test_vm.execute_instruction();
         assert_eq!(test_vm.registers[0], 257);
+    }
+
+    #[test]
+    fn test_opcode_div() {
+        let mut test_vm = VM::new();
+        // Load 5 in register 0
+        test_vm.program = vec![1, 0, 0, 5];
+        test_vm.pc = 0;
+        test_vm.execute_instruction();
+        // Load 2 in register 1
+        test_vm.program = vec![1, 1, 0, 2];
+        test_vm.pc = 0;
+        test_vm.execute_instruction();
+        // Check results of quotient in register 0 from the division
+        test_vm.program = vec![5, 0, 1, 0];
+        test_vm.pc = 0;
+        test_vm.execute_instruction();
+        assert_eq!(test_vm.registers[0], 2);
+        assert_eq!(test_vm.remainder, 1);
     }
 }
