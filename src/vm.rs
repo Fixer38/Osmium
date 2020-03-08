b2e62d5fa711b28c3117b6279001bf526ad0bb08
Fixer <fixer3838@live.fr>
2020-03-08 15:29:46 +0100

    Added comments for a few instructions match

--- src/vm.rs
+++ src/vm.rs
@@ -35,7 +35,7 @@ impl VM {
                 return false
             },
             Opcode::LOAD => {
-                // LOAD format: $0, $1
+                // LOAD format: LOAD $0, $1
                 // $0 register storing the loaded value
                 // $1 value to load into register on 16 bits
                 let register = self.next_8_bits() as usize; // usize cast for array index
@@ -43,7 +43,7 @@ impl VM {
                 self.registers[register] = number as i32;
             },
             Opcode::ADD => {
-                // ADD format: $1, $2, $3
+                // ADD format: ADD $1, $2, $3
                 // $1 and $2 operands to add from register
                 // $3 addition destination register
                 let operand1 = self.registers[self.next_8_bits() as usize];
@@ -51,7 +51,7 @@ impl VM {
                 self.registers[self.next_8_bits() as usize] = operand1 + operand2;
             },
             Opcode::SUB => {
-                // SUB format: $1, $2, $3
+                // SUB format: SUB $1, $2, $3
                 // $1 and $2 operands to substract from register
                 // $3 substraction destination register
                 let operand1 = self.registers[self.next_8_bits() as usize];
@@ -59,7 +59,7 @@ impl VM {
                 self.registers[self.next_8_bits() as usize] = operand1 - operand2;
             }
             Opcode::MUL => {
-                // MUL format: $1, $2, $3
+                // MUL format: MUL $1, $2, $3
                 // $1 and $2 operands to multiply from register
                 // $3 multiplication destination register
                 let operand1 = self.registers[self.next_8_bits() as usize];
@@ -67,7 +67,7 @@ impl VM {
                 self.registers[self.next_8_bits() as usize] = operand1 * operand2;
             }
             Opcode::DIV => {
-                // DIV format: $1, $2, $3
+                // DIV format: DIV $1, $2, $3
                 // $1 and $2 operands to multiply from register
                 // $3 division quotient destination register
                 // Remainder will contain remainder of the division => i32 allowed in the registers
@@ -77,10 +77,14 @@ impl VM {
                 self.remainder = (operand1 % operand2) as u32;
             }
             Opcode::JMP => {
+                // JMP format: JMP $1
+                // $1 register to get the pc from => Change pc according to its value
                 let target = self.registers[self.next_8_bits() as usize];
                 self.pc = target as usize;
             }
             Opcode::JMPF => {
+                // JMPF format: JMPF $1
+                // $1 register to get the value pc should be incremented
                 let value = self.registers[self.next_8_bits() as usize];
                 self.pc += value as usize;
             }
@@ -182,6 +186,7 @@ mod tests {
     
     #[test]
     fn test_opcode_jumpf() {
+        // Check if the pc got incremented properly
         let mut test_vm = VM::new();
         test_vm.registers[0] = 2;
         test_vm.program = vec![7, 0, 0, 0];
